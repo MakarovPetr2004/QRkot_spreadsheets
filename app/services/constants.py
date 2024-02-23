7f552ebf82e5a6df9af604da7fe7bf9d0d32efc9
Макаров Пётр <mr.ru.20042004@gmail.com>
2024-02-23 07:40:56 +0300

    added constants.py in services

---
+++ app/services/constants.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+from app.core.config import settings
+
+
+FORMAT = "%Y/%m/%d %H:%M:%S"
+
+SPREADSHEET_BODY = {
+    'properties': {'title': f'Отчёт на {datetime.now().strftime(FORMAT)}',
+                   'locale': 'ru_RU'},
+    'sheets': [{'properties': {'sheetType': 'GRID',
+                               'sheetId': 0,
+                               'title': 'Лист1',
+                               'gridProperties': {'rowCount': 100,
+                                                  'columnCount': 11}}}]
+}
+
+PERMISSIONS_BODY = {
+    'type': 'user',
+    'role': 'writer',
+    'emailAddress': settings.email
+}
+
+TABLE_VALUES = [
+    ['Отчёт от', datetime.now().strftime(FORMAT)],
+    ['Топ проектов по скорости закрытия'],
+    ['Название проекта', 'Время сбора', 'Описание']
+]
+
+UPDATE_BODY = {
+    'majorDimension': 'ROWS',
+    'values': TABLE_VALUES
+}
